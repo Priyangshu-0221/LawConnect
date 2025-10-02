dc97b284067a013568e0c3015df14e617ea103c5
Priyangshu-0221 <priyangshumondal821@gmail.com>
2025-10-02 13:17:19 +0530

    Ready for Deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectCloduinary();
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware())
+
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Welcome to LawConnect"));
 app.use(routes);
